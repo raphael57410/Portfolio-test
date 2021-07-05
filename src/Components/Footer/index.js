67059bd56d518cf33a39ed0d712879d7eed719a5
raph <demmerleraphael@hotmail.com>
2021-07-05 22:50:15 +0200

    change html attributes for link

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,4 +1,5 @@
 import './footer.scss';
+import { Link } from "react-router-dom";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -8,10 +9,10 @@ import MailIcon from '@material-ui/icons/Mail';
 const Footer = () => {
   return (
     <div className="footer--container">
-        <a href="https://github.com/raphael57410" target="_blank" rel="noreferrer"><GitHubIcon style={{ fontSize: 60 }}/></a>
-        <a href="https://www.facebook.com/raph.demmerle/" target="_blank" rel="noreferrer"><FacebookIcon style={{ fontSize: 60 }}/></a>
-        <a href="https://www.linkedin.com/in/rapha%C3%ABl-demmerle-9b62391b4/" target="_blank" rel="noreferrer"><LinkedInIcon style={{ fontSize: 60 }} /></a>
-        <a href="/contact"><MailIcon style={{ fontSize: 60 }} /></a>
+        <Link href="https://github.com/raphael57410" target="_blank" rel="noreferrer"><GitHubIcon style={{ fontSize: 60 }}/></Link>
+        <Link href="https://www.facebook.com/raph.demmerle/" target="_blank" rel="noreferrer"><FacebookIcon style={{ fontSize: 60 }}/></Link>
+        <Link href="https://www.linkedin.com/in/rapha%C3%ABl-demmerle-9b62391b4/" target="_blank" rel="noreferrer"><LinkedInIcon style={{ fontSize: 60 }} /></Link>
+        <Link href="/contact"><MailIcon style={{ fontSize: 60 }} /></Link>
     </div>
   );
 }
