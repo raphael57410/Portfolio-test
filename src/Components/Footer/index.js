6778d21bfde90528527ef1297be4de766c32fc46
raph <demmerleraphael@hotmail.com>
2021-07-05 22:59:05 +0200

    change the link for social in footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -9,10 +9,10 @@ import MailIcon from '@material-ui/icons/Mail';
 const Footer = () => {
   return (
     <div className="footer--container">
-        <Link href="https://github.com/raphael57410" target="_blank" rel="noreferrer"><GitHubIcon style={{ fontSize: 60 }}/></Link>
-        <Link href="https://www.facebook.com/raph.demmerle/" target="_blank" rel="noreferrer"><FacebookIcon style={{ fontSize: 60 }}/></Link>
-        <Link href="https://www.linkedin.com/in/rapha%C3%ABl-demmerle-9b62391b4/" target="_blank" rel="noreferrer"><LinkedInIcon style={{ fontSize: 60 }} /></Link>
-        <Link href="/contact"><MailIcon style={{ fontSize: 60 }} /></Link>
+        <a href="https://github.com/raphael57410" target="_blank" rel="noreferrer"><GitHubIcon style={{ fontSize: 60 }}/></a>
+        <a href="https://www.facebook.com/raph.demmerle/" target="_blank" rel="noreferrer"><FacebookIcon style={{ fontSize: 60 }}/></a>
+        <a href="https://www.linkedin.com/in/rapha%C3%ABl-demmerle-9b62391b4/" target="_blank" rel="noreferrer"><LinkedInIcon style={{ fontSize: 60 }} /></a>
+        <Link to="/contact"><MailIcon style={{ fontSize: 60 }} /></Link>
     </div>
   );
 }
