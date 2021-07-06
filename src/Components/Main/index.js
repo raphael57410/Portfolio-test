11f2417abf817132bd4279ffff4f0dda4d10511f
raph <demmerleraphael@hotmail.com>
2021-07-06 12:08:13 +0200

    add the name with animation

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,11 +1,26 @@
 import './main.scss';
-
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped'
 
 const Main = () => {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: false,
+      typeSpeed:  200,
+      strings: ['Developpeur Web'] });
+  },[]);
+
+
   return (
     <div className="main--content">
-      <div className="picture--container"></div>
-      <h1 className="title--content">Bienvenue sur mon portfolio</h1>
+      <div className="picture--container"></div>*
+      <div className='title--content'>
+        <h1 className="title">Bienvenue sur mon portfolio</h1>
+        <h2>Demmerle Raphael <span ref={textRef}></span></h2>
+      </div>
       <p className="text--content">
       Depuis mon plus jeune âge j'ai grandi avec la passion de
       l'informatique, j'ai toujours aimé tout ce qui touche au monde
